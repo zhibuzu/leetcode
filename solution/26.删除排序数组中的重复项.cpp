3d674ca93e54e7026a02a45a4e694b2c10bc5739
hulifa <hulifa@baidu.com>
2019-09-02 22:16:34 +0800

    add

--- solution/26.删除排序数组中的重复项.cpp
+++ solution/26.删除排序数组中的重复项.cpp
@@ -59,11 +59,12 @@ public:
             return 0;
         }
         int cursor = 0;
-        for (int i = 0; i < n; ++i) {
-            int cursor_val = nums[cursor];
+        int cursor_val = nums[cursor];
+        for (int i = 1; i < n; ++i) {
             if (nums[i] != cursor_val) {
                 ++cursor;
                 nums[cursor] = nums[i];
+                cursor_val = nums[cursor];
             }
         }
 
